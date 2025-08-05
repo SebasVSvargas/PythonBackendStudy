8325d7b173cd6c1ad93e933eeb6e1d21fff75276
Sebastian Vargas Suarez <svs.sebas@hotmail.com>
2025-08-04 23:26:36 -0500

    Decorator pattern

---
+++ Patrones/Decorator/1_decorator.py
@@ -0,0 +1,84 @@
+from abc import ABC, abstractmethod
+
+class Taco(ABC):
+    @abstractmethod
+    def price(self):
+        pass
+
+class BasicTaco(Taco):
+    def price(self):
+        return 5
+
+class TacoDecorator(Taco):
+    def __init__(self, taco:Taco):
+        self._taco = taco
+
+    def price(self):
+        return self._taco.price()
+    
+class DoubleMeatTaco(TacoDecorator):
+    def price(self):
+        return self._taco.price() + 4
+    
+class CheeseTaco(TacoDecorator):
+    def price(self):
+        return super().price() + 2
+    
+taco = BasicTaco()
+print(f"Taco sencillo: {taco.price()}")
+
+double_meat_taco = DoubleMeatTaco(
+                        CheeseTaco(
+                            BasicTaco()
+                        )
+                    )
+
+print(f"taco doble carne: {double_meat_taco.price()}")
+
+cheese_taco = CheeseTaco(BasicTaco())
+print(f"Taco con queso: {cheese_taco.price()}")
+
+triple_meat_taco = DoubleMeatTaco(double_meat_taco)
+print(f"Taco triple carne: {triple_meat_taco.price()}")
+
+# ------------------------------------- 
+
+# Componente base
+class Notificador(ABC):
+    @abstractmethod
+    def enviar(self, mensaje):
+        pass
+
+# Componente concreto
+class NotificadorBase(Notificador):
+    def enviar(self, mensaje):
+        print(f" Notificando: {mensaje}")
+
+# Decorador base
+class DecoradorNotificador(Notificador):
+    def __init__(self, notificador: Notificador):
+        self._notificador = notificador
+
+    def enviar(self, mensaje):
+        self._notificador.enviar(mensaje)
+
+# Decorador concreto: Email
+class NotificadorEmail(DecoradorNotificador):
+    def enviar(self, mensaje):
+        super().enviar(mensaje)
+        print(f" Enviando email con mensaje: {mensaje}")
+
+# Decorador concreto: SMS
+class NotificadorSMS(DecoradorNotificador):
+    def enviar(self, mensaje):
+        super().enviar(mensaje)
+        print(f" Enviando SMS con mensaje: {mensaje}")
+
+
+notificador = NotificadorSMS(
+                NotificadorEmail(
+                    NotificadorBase()
+                )
+            )
+
+notificador.enviar("¡Tu patrón Decorator funciona!")
